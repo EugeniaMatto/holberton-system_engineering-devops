c470b17c3bccc9c5021371b9eeba3f0d3c0e9936
EugeniaMatto <eugematto26@gmail.com>
2022-08-12 14:06:56 -0700

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # Using Puppet, create a file in /tmp
-
 file { '/tmp/school':
-    path       => /tmp/school,
-    permission => '0744',
-    owner      => 'www-data',
-    group      => 'www-data',
-    content    => 'I love Puppet',
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
